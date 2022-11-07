f538554f1840ac58f812b3f633d15b2ffb51b9e5
Surinder Singh Sandhu <surinder-singh.sandhu@capgemini.com>
2022-11-07 22:25:46 +0100

    !!!!

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello De-accelerate!!!
+          Hello De-accelerate!!!!
         </p>
         <a
           className="App-link"
